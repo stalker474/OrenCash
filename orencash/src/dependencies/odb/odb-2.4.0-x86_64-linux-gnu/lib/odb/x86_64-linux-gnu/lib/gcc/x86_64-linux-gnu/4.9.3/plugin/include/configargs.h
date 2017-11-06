dbcd28114ba503589a8880d81992fd414be99eda
Stalker474 <anton.possylkine@gmail.com>
2017-11-06 17:08:13 +0100

    crossplatform cmake configuration improvement

---
+++ orencash/src/dependencies/odb/odb-2.4.0-x86_64-linux-gnu/lib/odb/x86_64-linux-gnu/lib/gcc/x86_64-linux-gnu/4.9.3/plugin/include/configargs.h
@@ -0,0 +1,7 @@
+/* Generated automatically. */
+static const char configuration_arguments[] = "../gcc/configure --disable-bootstrap --enable-languages=c,c++ --disable-multiarch --disable-multilib --enable-shared --disable-libssp --disable-libgomp --disable-libsanitizer --disable-libcilkrts --enable-nls --disable-libstdcxx-pch --disable-libstdcxx-debug --prefix=/x86_64-linux-gnu --enable-plugin --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu --enable-cxx-flags=-fPIC CFLAGS=-O2 CXXFLAGS=-O2 LDFLAGS=-s 'CFLAGS_FOR_TARGET=-O2 -fPIC' 'CXXFLAGS_FOR_TARGET=-O2 -fPIC'";
+static const char thread_model[] = "posix";
+
+static const struct {
+  const char *name, *value;
+} configure_default_options[] = { { "cpu", "generic" }, { "arch", "x86-64" } };
