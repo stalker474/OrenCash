3ce13a879144ed694acc1f95693e4e65e9eab6a0
Stalker474 <anton.possylkine@gmail.com>
2017-11-07 11:21:33 +0100

    adding boost serialization as dependency instead of downloading last version

--- autoupdate.sh
+++ autoupdate.sh
@@ -9,7 +9,7 @@ cd orencash/src/dependencies
 
 wget https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.tar.gz
 echo "Extracting boost..."
-tar xvzf boost_1_65_1.tar.gz
+tar xvzf boost_1_65_1.tar.gz > /dev/null
 echo "Cleaning up"
 rm boost_1_65_1.tar.gz 
 cd boost_1_65_1
